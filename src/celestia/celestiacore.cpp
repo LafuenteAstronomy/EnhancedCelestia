103ff8d3ccbda9ee63e9526afc87e5362382836f
GurrenLagannTSS <54154710+GurrenLagannTSS@users.noreply.github.com>
2020-04-07 14:04:51 -0300

    Increased the FOV zoom limit further

--- src/celestia/celestiacore.cpp
+++ src/celestia/celestiacore.cpp
@@ -84,7 +84,7 @@ static const double MaximumTimeRate = 1.0e20;
 static const double MinimumTimeRate = 1.0e-20;
 static const float stdFOV = degToRad(45.0f);
 static const float MaximumFOV = degToRad(120.0f);
-static const float MinimumFOV = degToRad(0.000000000278f);
+static const float MinimumFOV = degToRad(1e-12f);
 static float KeyRotationAccel = degToRad(120.0f);
 static float MouseRotationSensitivity = degToRad(1.0f);
 
