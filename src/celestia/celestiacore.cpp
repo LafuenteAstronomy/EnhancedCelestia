bbe13c8d3fb1404ae4825c7d35fa347c38076c50
GurrenLagannTSS <54154710+GurrenLagannTSS@users.noreply.github.com>
2020-04-07 13:37:56 -0300

    Extended the FOV zoom and timewarp limits

--- src/celestia/celestiacore.cpp
+++ src/celestia/celestiacore.cpp
@@ -80,11 +80,11 @@ static const double FineTimeScaleFactor = 2.0;
 static const double fMaxKeyAccel = 20.0;
 static const float RotationBraking = 10.0f;
 static const float RotationDecay = 2.0f;
-static const double MaximumTimeRate = 1.0e15;
-static const double MinimumTimeRate = 1.0e-15;
+static const double MaximumTimeRate = 1.0e20;
+static const double MinimumTimeRate = 1.0e-20;
 static const float stdFOV = degToRad(45.0f);
 static const float MaximumFOV = degToRad(120.0f);
-static const float MinimumFOV = degToRad(0.001f);
+static const float MinimumFOV = degToRad(0.000000000278f);
 static float KeyRotationAccel = degToRad(120.0f);
 static float MouseRotationSensitivity = degToRad(1.0f);
 
